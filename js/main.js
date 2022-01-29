df2b993a865cb5125c5212a504b8e9e2f4f481c7
Dawit <dawityab@gmail.com>
2022-01-29 12:29:14 +0100

    showing events price

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// A function that shows all events price
+let calculateInvoice =  (price1,price2,price3,price4,price5,price6,price7,price8,price9) => {
+
+    console.log( `
+                  Stage 1 event 1 price is ${price1}
+                  Stage 1 event 2 price is ${price2}
+                  Stage 1 event 3 price is ${price3}
+                  Stage 2 event 1 price is ${price4}
+                  Stage 2 event 2 price is ${price5}
+                  Stage 2 event 3 price is ${price6}
+                  Stage 3 event 1 price is ${price7}
+                  Stage 3 event 2 price is ${price8}
+                  Stage 3 event 3 price is ${price9}
+                  `
+    )
+
+}
+calculateInvoice(140,20,249,140,20,249,140,20,249);
+
+// A function that shows the invoice of the customer
+
+let calculateInvoice = (band_2_Of_Stage_1,band_2_Stage_2,band_3_Stage_3){
+
+    
+}
