3836b6f714943d066ef233a6d2fa590d2c2e53b2
Dawit <dawityab@gmail.com>
2022-01-29 13:51:18 +0100

    student 10 per discount

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ let calculateInvoice =  (price1,price2,price3,price4,price5,price6,price7,price8
 }
 calculateInvoice(140,20,249,140,20,249,140,20,249);
 
-// A function that shows the invoice of the customer
+// A function that return the sum of the customer invoice
 
 let calculateInvoice1 = (band_2_Of_Stage_1,band_2_Of_Stage_2,band_3_Of_Stage_3) => {
 
@@ -29,19 +29,27 @@ let calculateInvoice1 = (band_2_Of_Stage_1,band_2_Of_Stage_2,band_3_Of_Stage_3) 
 
 console.log("The Sum of 3 customer invoice is " + calculateInvoice1(20,20,249));
 
-// A function that shows the invoices calculation
+// A calculation that shows the sum invoices 
 
 let price_3_Of_Stage_1 = 249; 
 let price_2_Of_Stage_2 = 20;
 let price_1_Of_Stage_3 = 140;  
 
-let sum_Of_invoices = price_3_Of_Stage_1 + price_2_Of_Stage_2 + price_3_Of_Stage_1;
+let sum_Of_Invoices = price_3_Of_Stage_1 + price_2_Of_Stage_2 + price_1_Of_Stage_3;
 
-console.log(`The sum of 3 different events invoice ${sum_Of_invoices}`);
+console.log(`The sum of 3 different events invoice ${sum_Of_Invoices}`);
 
-// A function that shows ths students discount invoice
+// A function that return a 10% discount invoice for students 
 
 let studentInvoice = (band_3_Of_Stage_1,band_1_Of_Stage_2,band_2_Of_Stage_3) => {
 
-    
+
+    let sum_of_student_Invoice = band_3_Of_Stage_1 + band_1_Of_Stage_2 + band_2_Of_Stage_3;
+
+    let ten_Percentage = sum_of_student_Invoice * 0.10 ;
+   
+    return sum_of_student_Invoice - ten_Percentage;
+
 }
+
+console.log("The final price for students with 10% discount is " + studentInvoice(249,140,20));
