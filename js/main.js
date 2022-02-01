42f3f96ce61913e4264dd199347626406ede05aa
Dawit <dawityab@gmail.com>
2022-02-01 19:11:12 +0100

    committed after a Feedback

--- js/main.js
+++ js/main.js
@@ -35,10 +35,11 @@ let price_3_Of_Stage_1 = 249;
 let price_2_Of_Stage_2 = 20;
 let price_1_Of_Stage_3 = 140;  
 
-let sum_Of_Invoices = price_3_Of_Stage_1 + price_2_Of_Stage_2 + price_1_Of_Stage_3;
 
-console.log(`The sum of 3 different events invoice ${sum_Of_Invoices}`);
 
+console.log(`The inovice of Stage 1 Event 3 invoice ${price_3_Of_Stage_1}`);
+console.log(`The inovice of Stage 2 Event 2 invoice ${price_2_Of_Stage_2}`);
+console.log(`The inovice of Stage 3 Event 1 invoice ${price_1_Of_Stage_3}`);
 // A function that return a 10% discount invoices for students 
 
 let studentInvoice = (band_3_Of_Stage_1,band_1_Of_Stage_2,band_2_Of_Stage_3) => {
@@ -52,4 +53,6 @@ let studentInvoice = (band_3_Of_Stage_1,band_1_Of_Stage_2,band_2_Of_Stage_3) => 
 
 }
 
-console.log("The final price for students with 10% discount is " + studentInvoice(249,140,20));
+console.log("The final price for student-1 with 10% discount is " + studentInvoice(249,140,20));
+console.log("The final price for student-2 with 10% discount is " + studentInvoice(75,90,120));
+console.log("The final price for student-3 with 10% discount is " + studentInvoice(130,190,60));
