7a45c56f9ab54b871f666884ba7bcdd197a1fa0f
Dawit <dawityab@gmail.com>
2022-02-02 10:49:07 +0100

    last commit after today's feedback

--- js/main.js
+++ js/main.js
@@ -1,22 +1,12 @@
 // A function that shows all events price
-let calculateInvoice =  (price1,price2,price3,price4,price5,price6,price7,price8,price9) => {
+let calculateInvoice =  (band1,band2,band3) => {
 
-    console.log( `
-                  Stage 1 event 1 price is ${price1}
-                  Stage 1 event 2 price is ${price2}
-                  Stage 1 event 3 price is ${price3}
-                  Stage 2 event 1 price is ${price4}
-                  Stage 2 event 2 price is ${price5}
-                  Stage 2 event 3 price is ${price6}
-                  Stage 3 event 1 price is ${price7}
-                  Stage 3 event 2 price is ${price8}
-                  Stage 3 event 3 price is ${price9}
-                  `
-    )
+ console.log(band1,band2,band3);
 
 }
-calculateInvoice(140,20,249,140,20,249,140,20,249);
-
+calculateInvoice(140,20,249);
+calculateInvoice(140,20,249);
+calculateInvoice(140,20,249);
 // A function that return the sum of 3 invoice
 
 let calculateInvoice1 = (band_2_Of_Stage_1,band_2_Of_Stage_2,band_3_Of_Stage_3) => {
@@ -30,16 +20,18 @@ let calculateInvoice1 = (band_2_Of_Stage_1,band_2_Of_Stage_2,band_3_Of_Stage_3) 
 console.log("The Sum of 3 invoice is " + calculateInvoice1(20,20,249));
 
 // A calculation that shows the invoices of different events 
+let calculateInvoice2 = (price_1_Of_Stage_1,price_1_Of_Stage_2,price_1_Of_Stage_3) =>{
+  
+    console.log(`The inovice of Stage 1 Event 1 invoice ${price_1_Of_Stage_1}`);
+    console.log(`The inovice of Stage 2 Event 1 invoice ${price_1_Of_Stage_2}`);
+    console.log(`The inovice of Stage 3 Event 1 invoice ${price_1_Of_Stage_3}`);
 
-let price_3_Of_Stage_1 = 249; 
-let price_2_Of_Stage_2 = 20;
-let price_1_Of_Stage_3 = 140;  
+};
+calculateInvoice2(249,20,140);
 
 
 
-console.log(`The inovice of Stage 1 Event 3 invoice ${price_3_Of_Stage_1}`);
-console.log(`The inovice of Stage 2 Event 2 invoice ${price_2_Of_Stage_2}`);
-console.log(`The inovice of Stage 3 Event 1 invoice ${price_1_Of_Stage_3}`);
+
 // A function that return a 10% discount invoices for students 
 
 let studentInvoice = (band_3_Of_Stage_1,band_1_Of_Stage_2,band_2_Of_Stage_3) => {
